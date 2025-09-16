d00a86a089579aa6723747f2b993b9e97463a711
[gameorstudy] <[jianyuxiong1998@gmail.com]>
2025-09-16 11:43:08 +0800

    feat: add eslint & prettier

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script>
-import { checkForUpdate } from "./utils/checkForUpdate";
-import { getTestData } from "./api/test";
+import { checkForUpdate } from './utils/checkForUpdate'
+import { getTestData } from './api/test'
 
+let x = 1
 export default {
-  onLaunch: function () {
-    checkForUpdate(); // 检测小程序是否更新
-    console.log("App Launch");
-    getTestData();
-  },
-  onShow: function () {
-    console.log("App Show");
-  },
-  onHide: function () {
-    console.log("App Hide");
-  },
-};
+	onLaunch: function () {
+		checkForUpdate() // 检测小程序是否更新
+		console.log('App Launch')
+		getTestData()
+	},
+	onShow: function () {
+		console.log('App Show')
+	},
+	onHide: function () {
+		console.log('App Hide')
+	}
+}
 </script>
 
 <style>
--- src/request/index.js
+++ src/request/index.js
@@ -1,29 +1,30 @@
+let x = 1
 function request(url, data, config = {}) {
-  return new Promise((resolve, reject) => {
-    uni.request({
-      url: `${import.meta.env.VITE_BASE_URL}${url}`,
-      data,
-      method: 'POST',
-      success: function (res) {
-        const { code, msg, data } = res
-        // 接口是否正确处理
-        if (code !== '200') {
-          reject(msg)
-          uni.showToast({
-            title: msg,
-            icon: 'none'
-          })
-          return
-        }
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: `${import.meta.env.VITE_BASE_URL}${url}`,
+			data,
+			method: 'POST',
+			success: function (res) {
+				const { code, msg, data } = res
+				// 接口是否正确处理
+				if (code !== '200') {
+					reject(msg)
+					uni.showToast({
+						title: msg,
+						icon: 'none'
+					})
+					return
+				}
 
-        resolve(data)
-      },
-      fail: function (err) {
-        console.error('err', err)
-      },
-      ...config
-    })
-  })
+				resolve(data)
+			},
+			fail: function (err) {
+				console.error('err', err)
+			},
+			...config
+		})
+	})
 }
 
 export default request
