7edab6fda4d2074a41600e14cb96f6881e3602e7
阿离 <1161653413@qq.com>
2025-09-16 18:11:25 +0800

    feat: add mine

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="usercenter">
+		<view class="nav-bar">导航栏</view>
+		<view>客户中心</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {}
+	},
+	onLoad() {},
+	methods() {}
+}
+</script>
+
+<style>
+</style>
